bb348e024d20df63afc7f1384249385e3a83322e
Brandon Little <brandonlittle1999@hotmail.com>
2020-05-09 18:15:53 +0100

    Added fetch functionality to add the image and the joke/statement each time the user clicks on the 'Another' button.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,25 @@
 let div = document.getElementById("joke");
+let image = document.getElementById("icon");
+let button = document.getElementById("button")
 
+let url = 'https://api.chucknorris.io/jokes/random';
+let chuckArray = [];
 
+function chuckAPI(){
+    fetch(url).then( (response) => {
+        return response.json();
+    }).then( (data) => {
+        let icon = data.icon_url;
+        let joke = data.value;
+        div.innerText = joke;
+        image.src = icon;
+        chuckArray.push(joke);
+    }).catch((error) => {
+        console.log("Error: " + error);
+        let randArrayNum = Math.floor(Math.random() * chuckArray.length);
+        div.innerText = chuckArray[randArrayNum];
+    });
+}
+
+window.addEventListener("load", chuckAPI);
+button.addEventListener("click", chuckAPI);
